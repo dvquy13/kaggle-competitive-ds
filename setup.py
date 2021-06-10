aa5e7b8b38d994f9390e71a7f51a846f68d31db9
Quy Dinh <quy.dinh3195@gmail.com>
2021-06-10 13:53:09 +0700

    Fix module name

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="ccds",
     packages=find_packages(),
-    version='0.1.0',
-    description='Coursera Kaggle Competitive Data Science',
-    author='dvquys',
-    license='',
+    version="0.1.0",
+    description="Coursera Kaggle Competitive Data Science",
+    author="dvquys",
+    license="",
 )
